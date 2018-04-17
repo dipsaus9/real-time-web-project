a11113b6fe9eda0234d6cc970005949c013148d3
Dennis <dennis.spierenburg@hotmail.com>
2018-04-17 22:23:19 +0200

    it is working, well at least the gyro locations

--- server/sockets.js
+++ server/sockets.js
@@ -1,10 +1,43 @@
 const sockets = {
   init: function(io){
+    let users = [];
+    let positions = [
+      {
+        player: 1,
+        position: 50,
+        score: 0,
+      },
+      {
+        player: 2,
+        position: 50,
+        score: 0,
+      }
+    ];
     io.on('connection', function(socket){
+      if(users.length < 3){
+        users.push(socket.id);
+        io.to(socket.id).emit('start', socket.id);
+        io.emit('users', users);
+      }
+      else{
+        io.emit('tooMany', 'message');
+      }
       socket.on('cords', function(cord){
-        io.emit('cords', cord);
+        let id = socket.id;
+        let indexOf = users.indexOf(socket.id);
+        if(indexOf == 1){
+          positions[0].position = cord;
+        }
+        else if(indexOf == 2){
+          positions[1].position = cord;
+        }
+        io.emit('cords', positions);
       });
       socket.on('disconnect', function(){
+        let indexOf = users.indexOf(socket.id);
+        if(indexOf > -1){
+          users.splice(indexOf, 1);
+        }
       });
     });
   }
--- src/js/app.js
+++ src/js/app.js
@@ -2,30 +2,60 @@ const socket = io();
 
 
 window.addEventListener('deviceorientation', handleOrientation, true);
-let previouwPosition = 1000;
+let user;
+let number = -1;
+socket.on('start', function(id){
+  user = id;
+});
+socket.on('users', function(users){
+  let indexOf = users.indexOf(user);
+});
+let positionTouch = 50;
+window.addEventListener('keydown', function(e){
+  if(number !== 0){
+    if(e.key == 'ArrowDown'){
+      if(positionTouch < 100){
+        positionTouch += 3;
+        socket.emit('cords', positionTouch);
+      }
+    }
+    else if(e.key == 'ArrowUp'){
+      if(positionTouch > 0){
+        positionTouch -= 3;
+        socket.emit('cords', positionTouch);
+      }
+    }
+  }
+});
+let previouwPosition = 180;
 function handleOrientation(event) {
   var beta = event.beta;
   var difference = previouwPosition - beta;
   //maybe change the fire limit for later?
   if(difference > 0 || difference < 0){
     previouwPosition = beta;
-    socket.emit('cords', beta);
+    //cords can be from -180 to 180 create scale from this
+    //create scale from 0 to 360
+    let calcCord = beta + 180;
+    //create scale from 0 to 100, then get the scale from 35 to 65, this will be the position of the element in %
+    let position = ((calcCord * 100 / 360) - 35) / 0.3;
+    if(position < 0){
+      position = 0;
+    }
+    else if(position > 100){
+      position = 100;
+    }
+    if(number !== 0){
+    //emit a position from 0% to 100%
+      socket.emit('cords', position);
+    }
   }
 }
-var element = document.querySelector('#ball');
+var element1 = document.querySelector('#player1');
+var element2 = document.querySelector('#player2');
 socket.on('cords', function(cord){
-  //cords can be from -180 to 180 create scale from this
-  //create scale from 0 to 360
-  let calcCord = cord + 180;
-  //create scale from 0 to 100, then get the scale from 35 to 65, this will be the position of the element in %
-  let position = ((calcCord * 100 / 360) - 35) / 0.3;
-  console.log(position);
-  if(position < 0){
-    position = 0;
-  }
-  else if(position > 100){
-    position = 100;
-  }
-  position = position + '%';
-  element.style.top = position;
+  let firstPosition = cord[0].position + '%';
+  element1.style.top = firstPosition;
+  let secondPosition = cord[1].position + '%';
+  element2.style.top = secondPosition;
 });
